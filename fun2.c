2357d25acd62a198c69d1f0fb0197225edd5208f
linkema <supermlk@163.com>
2019-09-30 00:00:14 -0700

    冒泡排序

---
+++ fun2.c
@@ -0,0 +1,40 @@
+#include<stdio.h> 
+
+void main() 
+
+{ 
+
+int a[10]; 
+
+int i,j,t; 
+
+printf("input 10 numbers:\n"); 
+
+for(i=0;i<10;i++) 
+
+scanf("%d",&a[i]);
+
+for(j=0;j<9;j++) /*进行9次循环 实现9趟比较*/ 
+
+for(i=0;i<9-j;i++) /*在每一趟中进行9-j次比较*/ 
+
+if(a[i]>a[i+1]) /*相邻两个数比较,想降序只要改成a[i]<a[i+1]*/ 
+
+{ 
+
+t=a[i]; 
+
+a[i]=a[i+1]; 
+
+a[i+1]=t; 
+
+} 
+
+printf("the sorted numbers:\n"); 
+
+for(i=0;i<10;i++) 
+
+printf(" %d",a[i]);
+
+
+}
