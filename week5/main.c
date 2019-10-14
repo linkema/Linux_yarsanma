6bc690de5c31e23f6c72b2e8d56e7e19fc29ec84
linkema <supermlk@163.com>
2019-10-13 23:59:01 -0700

    静动态链接task5

--- week5/main.c
+++
@@ -1,6 +0,0 @@
-#include"/home/rlk/linux_yarsanma/week5/my.h"
-int main()
-{
-	fun();
-	return 0;
-}
---
+++ week5/staticlib/main.c
@@ -0,0 +1,6 @@
+#include"/home/rlk/linux_yarsanma/week5/staticlib/my.h"
+int main()
+{
+	fun();
+	return 0;
+}
